2de7c34111a67b3e22793e4f18d5a7dcf79bfdb1
Angamuthu <Angamuthu@DESKTOP-R98BTSV>
2023-03-23 06:45:47 +0530

    first commit

---
+++ src/Gqtcodes/program1.java
@@ -0,0 +1,18 @@
+package Gqtcodes;
+
+public class program1 {class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
